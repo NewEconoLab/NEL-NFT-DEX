80f2f3ef54a56851463fb5a73fde6282e7aa2bf0
qingmingzi <matrix3345@hotmail.com>
2019-04-30 16:44:27 +0800

    技术验证初版，实现NFT打包、NFT授权验证

---
+++ NEL-NFT-DEX/NEL-NFT-DEX/Contract1.cs
@@ -0,0 +1,244 @@
+﻿using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework.Services.Neo;
+using Neo.SmartContract.Framework.Services.System;
+using System;
+using System.ComponentModel;
+using System.Numerics;
+using Helper = Neo.SmartContract.Framework.Helper;
+
+/*
+交易类型：
+
+NFT兑NEP5：
+一口价
+荷兰拍
+带价求购
+
+NFT兑NFT：
+NFT交换邀约（不清楚兑换物） N-N
+带NFT求购（明确兑换物） N-N
+
+NFTpackage{
+    [
+        {
+            scriptHash,
+            tokenID
+        },
+        {
+            scriptHash,
+            tokenID
+        }
+    ]domainCenterHash
+}
+ 
+*/
+
+namespace NEL_NFT_DEX
+{
+    public class Contract1 : SmartContract
+    {
+        public class Token
+        {
+            public Token()
+            {
+                token_id = 0;
+                owner = new byte[0];
+                approved = new byte[0];
+                properties = "";
+                uri = "";
+                rwProperties = "";
+            }
+            //不能使用get set
+
+            public BigInteger token_id;// { get; set; } //代币ID
+            public byte[] owner;//  { get; set; } //代币所有权地址
+            public byte[] approved;//  { get; set; } //代币授权处置权地址
+            public string properties;//  { get; set; } //代币只读属性
+            public string uri;//  { get; set; } //代币URI链接
+            public string rwProperties;//  { get; set; } //代币可修改属性
+        }
+
+        public class NFT {
+            public byte[] scriptHash;
+            public BigInteger tokenID;
+        }
+
+        public class Order {
+            public byte[] orderID; //=TXID
+            public byte[] seller;
+            public NFT[] nFTs;
+            public byte[] assetID;
+            public BigInteger price;
+            public byte[] buyer;
+        }
+
+        //动态合约调用委托
+        delegate object deleDyncall(string method, object[] arr);
+
+        //事件
+        public delegate void deleSellOrderCreated(byte[] orderID, Order order);
+        [DisplayName("sellOrderCreated")]
+        public static event deleSellOrderCreated onSellOrderCreated;
+
+        public delegate void deleSellOrderSold(byte[] orderID, Order order);
+        [DisplayName("sellOrderSold")]
+        public static event deleSellOrderSold onSellOrderSold;
+
+        public static bool checkNFTs(NFT[] nFTs)
+        {
+            foreach (NFT nft in nFTs)
+            {
+                //构造入参
+                object[] input = new object[1];
+                input[0] = nft.tokenID;
+
+                //动态调用NFT合约
+                deleDyncall dyncall = (deleDyncall)nft.scriptHash.ToDelegate();
+                Token resultToken = (Token)dyncall("token", input);
+
+                //验证所有权
+                if (!Runtime.CheckWitness(resultToken.owner)) return false;
+
+                //验证是否已经授权给本合约
+                //byte[] resultAllowance = (byte[])dyncall("allowance", input);
+                if (resultToken.approved.AsBigInteger() != ExecutionEngine.ExecutingScriptHash.AsBigInteger()) return false;
+            }
+
+            return true;
+        }
+
+        public static bool transferNEP5(byte[] from, byte[] to, byte[] assetHash, BigInteger amount) {
+            //多判断总比少判断好
+            if (amount <= 0)
+                return false;
+            if (from.Length != 20 || to.Length != 20)
+                return false;
+
+            //构造入参
+            object[] transInput = new object[3];
+            transInput[0] = from;
+            transInput[1] = to;
+            transInput[2] = amount;
+
+            //动态调用执行转账
+            deleDyncall dyncall = (deleDyncall)assetHash.ToDelegate();
+            bool result = (bool)dyncall("transfer", transInput);
+
+            return result;
+        }
+
+        public static bool NFTtransferOwnership(NFT[] nFTs, byte[] to) {
+            foreach (NFT nft in nFTs)
+            {
+                //构造入参
+                object[] input = new object[2];
+                input[0] = to;
+                input[1] = nft.tokenID;
+
+                deleDyncall dyncall = (deleDyncall)nft.scriptHash.ToDelegate();
+                //转移所有权
+                bool resultTransferFrom = (bool)dyncall("transferFrom", input);
+                if (!resultTransferFrom) return false;//只要有一个失败就失败
+            }
+
+            return true;
+        }
+
+        //NFT(组)售卖创建
+        public static bool sellNFT(byte[] seller, byte[] nFTsB, byte[] assetID, BigInteger price) {
+            NFT[] nFTs = Helper.Deserialize(nFTsB) as NFT[];
+
+            if (!Runtime.CheckWitness(seller)) return false;
+            if (assetID.Length != 20) return false;
+            if (price < 0) return false;
+
+            //检查NFT组是否为调用者所有，是否所有NFT都已授权合约
+            if (!checkNFTs(nFTs)) return false;
+
+            byte[] TXID = (ExecutionEngine.ScriptContainer as Transaction).Hash;
+
+            Order order = new Order();
+            order.orderID = TXID;
+            order.seller = seller;
+            order.nFTs = nFTs;
+            order.assetID = assetID;
+            order.price = price;
+            order.buyer = new byte[0];
+
+            StorageMap sellOrderMap = Storage.CurrentContext.CreateMap("sellOrder");
+            sellOrderMap.Put(order.orderID, Helper.Serialize(order));
+            onSellOrderCreated(order.orderID, order);
+
+            return true;
+        }
+
+        //NFT出售订单购买
+        public static bool buySellOrder(byte[] orderID,byte[] buyer){
+            StorageMap sellOrderMap = Storage.CurrentContext.CreateMap("sellOrder");
+            var data = sellOrderMap.Get(orderID);
+            if(data.Length > 0) {
+                Order order = Helper.Deserialize(data) as Order;
+
+                
+
+                //执行NFT所有权转移
+                if (!NFTtransferOwnership(order.nFTs, buyer)) {
+                    //此处需要强制抛出异常触发回滚
+                    throw new Exception("NFTtransferOwnership Fail！");
+                    //return false;
+                }
+
+
+                //执行转账
+                if (!transferNEP5(buyer, order.seller, order.assetID, order.price)) {
+                    //此处需要强制抛出异常触发回滚
+                    throw new Exception("transferNEP5 Fail！");
+                    //return false;
+                } 
+
+                order.buyer = buyer;
+                sellOrderMap.Put(orderID, Helper.Serialize(order));
+                onSellOrderSold(orderID, order);
+            }
+            else return false;
+
+            return true;
+        }
+
+        //NFT（组）求购
+        public static bool buyNFT(byte[] buyer,NFT[] nFTs, byte[] assetID, BigInteger price)
+        {
+
+            return false;
+        }
+
+        //NFT以物易物
+        public static bool barterNFT(NFT[] nFTsSell, NFT[] nFTsBuy)
+        {
+
+            return false;
+        }
+
+        public static object Main(string operation, object[] args)
+        {
+            //UTXO转账转入转出都不允许
+            if (Runtime.Trigger == TriggerType.Verification || Runtime.Trigger == TriggerType.VerificationR)
+            {
+                return false;
+            }
+            else if (Runtime.Trigger == TriggerType.Application)
+            {
+                if (operation == "sellNFT")
+                {
+                    return sellNFT((byte[]) args[0],(byte[]) args[1],(byte[]) args[2],(BigInteger) args[3]);
+                }
+                if (operation == "buySellOrder")
+                {
+                    return buySellOrder((byte[])args[0], (byte[])args[1]);
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ NEL-NFT-DEX/NEL-NFT-DEX/test0.cs
@@ -0,0 +1,193 @@
+﻿using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework.Services.Neo;
+using Neo.SmartContract.Framework.Services.System;
+using System;
+using System.ComponentModel;
+using System.Numerics;
+using Helper = Neo.SmartContract.Framework.Helper;
+
+//0x394b3ccd59d7bfb00f5d101fd6e872fe159ca069 test042902
+
+/*
+交易类型：
+
+NFT兑NEP5：
+一口价
+荷兰拍
+带价求购
+
+NFT兑NFT：
+NFT交换邀约（不清楚兑换物） N-N
+带NFT求购（明确兑换物） N-N
+
+NFTpackage{
+    [
+        {
+            scriptHash,
+            tokenID
+        },
+        {
+            scriptHash,
+            tokenID
+        }
+    ]domainCenterHash
+}
+ 
+*/
+
+namespace NEL_NFT_DEX
+{
+    public class test0 : SmartContract
+    {
+        public class Token
+        {
+            public Token()
+            {
+                token_id = 0;
+                owner = new byte[0];
+                approved = new byte[0];
+                properties = "";
+                uri = "";
+                rwProperties = "";
+            }
+            //不能使用get set
+
+            public BigInteger token_id;// { get; set; } //代币ID
+            public byte[] owner;//  { get; set; } //代币所有权地址
+            public byte[] approved;//  { get; set; } //代币授权处置权地址
+            public string properties;//  { get; set; } //代币只读属性
+            public string uri;//  { get; set; } //代币URI链接
+            public string rwProperties;//  { get; set; } //代币可修改属性
+        }
+
+        static readonly byte[] sh = Helper.ToScriptHash("AbpkfkKqxzYuoJECDUJqzq3UXaZ2BBZGXJ");
+
+        public delegate void deleLog(BigInteger rowID,string infoName, object info);
+        [DisplayName("log")]
+        public static event deleLog onLog;
+
+        //动态合约调用委托
+        delegate object deleDyncall(string method, object[] arr);
+
+        public class NFT
+        {
+            public byte[] scriptHash;
+            public BigInteger tokenID;
+        }
+
+        public static NFT[] getNFTs() {
+            StorageMap nFTsMap = Storage.CurrentContext.CreateMap("nFTs");
+            byte[] data = nFTsMap.Get("nFTs");
+            if (data.Length > 0)
+            {
+                return Helper.Deserialize(data) as NFT[];
+            }
+            else {
+               
+                NFT[] nFTs = new NFT[] {
+                    new NFT {
+                        scriptHash= sh,
+                        tokenID = 5
+                        },
+                    new NFT {
+                        scriptHash= sh,
+                        tokenID = 20
+                        },
+                    new NFT {
+                        scriptHash= sh,
+                        tokenID = 300
+                        }
+                };
+
+                return nFTs;
+            }
+        }
+
+        public static byte[] getNFTsBytes() {
+            return Helper.Serialize(getNFTs());
+        }
+
+        public static bool setNFTs(byte[] nFTsB) {
+            NFT[] nFTs = Helper.Deserialize(nFTsB) as NFT[];
+            StorageMap nFTsMap = Storage.CurrentContext.CreateMap("nFTs");
+            nFTsMap.Put("nFTs",Helper.Serialize(nFTs));
+
+            return true;
+        }
+
+        public static bool clearNFTs() {
+            StorageMap nFTsMap = Storage.CurrentContext.CreateMap("nFTs");
+            nFTsMap.Delete("nFTs");
+
+            return true;
+        }
+
+
+
+        public static bool checkNFTs(NFT[] nFTs)
+        {
+            foreach (NFT nft in nFTs)
+            {
+                //构造入参
+                object[] input = new object[1];
+                input[0] = nft.tokenID;
+
+                //动态调用NFT合约
+                deleDyncall dyncall = (deleDyncall)nft.scriptHash.ToDelegate();
+                Token resultToken = (Token)dyncall("token", input);
+
+                //验证所有权
+                if (!Runtime.CheckWitness(resultToken.owner)) return false;
+
+                onLog(149, "approved", resultToken.approved);
+                onLog(149, "approvedBI", resultToken.approved.AsBigInteger());
+                onLog(149, "ExecutingScriptHash", ExecutionEngine.ExecutingScriptHash);
+                onLog(149, "ExecutingScriptHashBI", ExecutionEngine.ExecutingScriptHash.AsBigInteger());
+
+                //验证是否已经授权给本合约
+                //byte[] resultAllowance = (byte[])dyncall("allowance", input);
+                if (resultToken.approved.AsBigInteger() != ExecutionEngine.ExecutingScriptHash.AsBigInteger()) return false;
+            }
+
+            return true;
+        }
+
+        public static object Main(string operation, object[] args)
+        {
+            //UTXO转账转入转出都不允许
+            if (Runtime.Trigger == TriggerType.Verification || Runtime.Trigger == TriggerType.VerificationR)
+            {
+                return false;
+            }
+            else if (Runtime.Trigger == TriggerType.Application)
+            {
+                if (operation == "setNFTs")
+                {
+                    return setNFTs((byte[])args[0]);
+                }
+                if (operation == "getNFTs")
+                {
+                    return getNFTs();
+                }
+                if (operation == "getNFTsBytes")
+                {
+                    return getNFTsBytes();
+                }
+                if (operation == "clearNFTs")
+                {
+                    return clearNFTs();
+                }
+                if (operation == "checkNFTsByBytes")
+                {
+                    return checkNFTs(Helper.Deserialize((byte[])args[0]) as NFT[]);
+                }
+                if (operation == "checkNFTsByStorage")
+                {
+                    return checkNFTs(getNFTs());
+                }
+            }
+
+            return false;
+        }
+    }
+}
